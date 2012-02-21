493815f9d03bcbe227e354edd0afaca82c70ce34
Kohei Hasegawa <ameutau@gmail.com>
2012-02-22 01:34:25 +0900

    setup rspec

---
+++ spec/spec_helper.rb
@@ -0,0 +1,8 @@
+# Requires supporting files with custom matchers and macros, etc,
+# in ./support/ and its subdirectories.
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+
+RSpec.configure do |config|
+  # config.mock_with :rr
+  config.mock_with :rspec
+end
